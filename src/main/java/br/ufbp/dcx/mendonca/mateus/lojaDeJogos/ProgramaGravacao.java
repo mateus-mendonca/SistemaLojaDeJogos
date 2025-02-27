315d59429406c91b6ce966bbff2048e80a3da6a1
Mateus Mendonça <mateusmendonca810@gmail.com>
2025-02-27 10:53:39 -0300

    Update ProgramaGravacao.java

--- src/main/java/br/ufbp/dcx/mendonca/mateus/lojaDeJogos/ProgramaGravacao.java
+++ src/main/java/br/ufbp/dcx/mendonca/mateus/lojaDeJogos/ProgramaGravacao.java
@@ -1,37 +1,47 @@
 package br.ufbp.dcx.mendonca.mateus.lojaDeJogos;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
+
+import static br.ufbp.dcx.mendonca.mateus.lojaDeJogos.Genero.*;
 
 public class ProgramaGravacao {
     public static void main(String [] args) {
-
-        Jogo j1 = new Jogo("Call of Duty: Black Ops",
+        List<Jogo> jogos = new ArrayList<>();
+        /*Jogo j1 = new Jogo("Call of Duty: Black Ops",
                 "Call of Duty: Black Ops é um jogo eletrônico de " +
-                        "tiro em primeira pessoa desenvolvido pela Treyarch.", "01", Genero.TIRO, 50, 999);
+                        "tiro em primeira pessoa desenvolvido pela Treyarch.", TIRO, 50, 999);
         Jogo j2 = new Jogo("The Last of Us", "The Last of Us é um jogo eletrônico de ação-aventura " +
                 "e sobrevivência desenvolvido pela Naughty Dog" +
-                " e publicado pela Sony Computer Entertainment.", "02", Genero.ACAO_AVENTURA, 120, 999);
+                " e publicado pela Sony Computer Entertainment.", ACAO_AVENTURA, 120, 999);
 
-        List<Jogo> jogos = new LinkedList<>();
+
         jogos.add(j1);
-        jogos.add(j2);
-
+        jogos.add(j2);*/
+        //List<Jogo> jogos = new ArrayList<>();
         GravadorDeJogos gravadorDeJogos = new GravadorDeJogos();
         System.out.println("Recuperando jogos...");
         try {
-            Collection<Jogo> jogosAchados = gravadorDeJogos.recuperarJogos();
+            gravadorDeJogos.recuperarJogos();
             System.out.println("Jogos achados:");
-            for (Jogo j: jogosAchados) {
+            for (Jogo j: jogos) {
                 System.out.println(j.toString());
             }
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
 
+
+
+        /*try {
             gravadorDeJogos.gravaJogos(jogos);
-
+            System.out.println("Jogos salvos com sucesso. ");
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
+
+
     }
 }
